8bb2326e72501b2357722640e04d53f8a9206db1
Francois Ledoyen <ledoyen202@C302L-G20P13.png.unicaen.fr>
2025-01-30 18:05:01 +0100

    test mistral

---
+++ src/expes/tests/models/test_llama.py
@@ -0,0 +1,32 @@
+import unittest
+
+import torch
+from transformers import LlamaConfig, LlamaForCausalLM
+from transformers.testing_utils import torch_device
+
+from expes.tests.models.base import ModelBaseTestMixin
+from expes.tests.utils import ids_tensor
+
+
+class TestLlama(ModelBaseTestMixin, unittest.TestCase):
+    def build_model(self):
+        config = LlamaConfig(
+            hidden_size=32,
+            num_hidden_layers=5,
+            num_attention_heads=4,
+            intermediate_size=37,
+            hidden_act="gelu",
+            pad_token_id=0,
+        )
+        model = LlamaForCausalLM(config)
+        return model
+
+    def inputs(self, bsz=1, n_tasks=0):
+        inputs = {
+            "input_ids": ids_tensor((bsz, 128), 1000).to(torch_device),
+            "labels": ids_tensor((bsz, 128), 1000).to(torch_device),
+        }
+        if n_tasks > 0:
+            inputs["task_ids"] = torch.randint(0, n_tasks, (bsz,))
+
+        return inputs
