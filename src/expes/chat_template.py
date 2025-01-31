8a40d3ac08310adf6f8cb7793eb6981992fa7e8f
Francois Ledoyen <ledoyen202@C302L-G20P13.png.unicaen.fr>
2025-01-31 20:36:30 +0100

    add chat template class

--- src/expes/chat_template.py
+++ src/expes/chat_template.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import partial
 
 
 @dataclass
@@ -8,12 +9,29 @@ class ChatTemplate:
     chat_template = """
         {%- for message in messages %}
             {%- if message['role'] == 'user' %}
-                {{- '### Input:' + message['content'] + '### Output:'}}
+                {{- input_prefix + ' ' + message['content'] }}
             {%- elif message['role'] == 'assistant' %}
-                {{- ' '  + message['content'] + ' ' + eos_token }}
+                {{- ' ' + output_prefix + ' ' + message['content'] + ' ' + eos_token }}
+            {%- endif %}
+
+            {%- if add_generation_prompt %}
+                {{- ' ' + output_prefix }}
             {%- endif %}
         {%- endfor %}
     """
+
+    def apply_to_tokenizer(self, tokenizer):
+        tokenizer.chat_template = self.chat_template
+        tokenizer.input_prefix = self.input_prefix
+        tokenizer.output_prefix = self.output_prefix
+
+        tokenizer.apply_chat_template = partial(
+            tokenizer.apply_chat_template,
+            input_prefix=tokenizer.input_prefix,
+            output_prefix=tokenizer.output_prefix,
+        )
+
+        return tokenizer
 
 
 causal_chat_template = ChatTemplate()
--- src/expes/tests/test_chat_template.py
+++ src/expes/tests/test_chat_template.py
@@ -23,15 +23,47 @@ class TestChatTemplate(unittest.TestCase):
         ]
         return messages
 
-    def test_apply_template_input_answer(self):
+    def test_apply_template_with_answer(self):
         input_text = lorem.sentence()
         answer = lorem.sentence()
         input = self.build_message(input=input_text, answer=answer)
         formatted = self.tokenizer.apply_chat_template(
-            input, tokenize=False
+            input,
+            tokenize=False,
+            input_prefix=causal_chat_template.input_prefix,
+            output_prefix=causal_chat_template.output_prefix,
         ).strip()
 
         assert input_text in formatted
         assert answer in formatted
-        expected = f"{causal_chat_template.input_prefix}{input_text}{causal_chat_template.output_prefix} {answer} {self.tokenizer.eos_token}"
+        expected = f"{causal_chat_template.input_prefix} {input_text} {causal_chat_template.output_prefix} {answer} {self.tokenizer.eos_token}"
+        assert formatted == expected
+
+    def test_apply_template_without_answer(self):
+        input_text = lorem.sentence()
+        input = self.build_message(input=input_text)
+        formatted = self.tokenizer.apply_chat_template(
+            input,
+            tokenize=False,
+            input_prefix=causal_chat_template.input_prefix,
+            output_prefix=causal_chat_template.output_prefix,
+        ).strip()
+
+        assert input_text in formatted
+        expected = f"{causal_chat_template.input_prefix} {input_text}"
+        assert formatted == expected
+
+    def test_apply_template_without_answer_with_generation_prompt(self):
+        input_text = lorem.sentence()
+        input = self.build_message(input=input_text)
+        formatted = self.tokenizer.apply_chat_template(
+            input,
+            tokenize=False,
+            add_generation_prompt=True,
+            input_prefix=causal_chat_template.input_prefix,
+            output_prefix=causal_chat_template.output_prefix,
+        ).strip()
+
+        assert input_text in formatted
+        expected = f"{causal_chat_template.input_prefix} {input_text} {causal_chat_template.output_prefix}"
         assert formatted == expected
