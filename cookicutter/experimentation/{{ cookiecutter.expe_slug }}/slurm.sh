1c1d15a349c86a447d13a9bcf3fb54a66d3758c8
Francois Ledoyen <ledoyen202@C302L-G20P13.png.unicaen.fr>
2025-03-01 17:14:17 +0100

    update slurm sh

--- cookicutter/experimentation/{{ cookiecutter.expe_slug }}/slurm.sh
+++ cookicutter/experimentation/{{ cookiecutter.expe_slug }}/slurm.sh
@@ -17,7 +17,7 @@
 
 job_dir=/dlocal/run/$SLURM_JOB_ID
 
-# Set TMPDIR to work in /dlocal/run instead of lustre
+# Set TMPDIR to work in /dlocal/run instead of lustre
 tmpdir=$job_dir/tmp
 mkdir -p $tmpdir
 export TMPDIR=$tmpdir
@@ -25,7 +25,39 @@ export TMPDIR=$tmpdir
 # Set results location
 results_dir=$job_dir/results
 mkdir -p $results_dir
-ln -s $results_dir results
+
+# Parse arguments
+overwrite=false
+continue_run=false
+for arg in "$@"; do
+    case $arg in
+        --overwrite)
+            overwrite=true
+            ;;
+        --continue)
+            continue_run=true
+            ;;
+    esac
+done
+
+# Handle overwrite option
+if [ "$overwrite" = true ]; then
+    [ -L results ] && rm results
+fi
+
+# Handle continue option
+if [ "$continue_run" = true ]; then
+    if [ -d "./results" ]; then
+        actual_result_path=$(readlink ./results)
+        rm -rf $results_dir # remove /dlocal/run/newid/results
+        ln -sf $actual_result_path $results_dir # link prev results dir in /dlocal/run/newid
+        rm -rf $tmpdir # same for tmp dir
+        ln -sf $(dirname $actual_result_path)/tmp $tmpdir
+    fi
+else
+    # Create symbolic link if not overwritten
+    ln -s $results_dir results
+fi
 
 module load aidl/pytorch/2.0.0-cuda11.7
 srun python ./run.py \
