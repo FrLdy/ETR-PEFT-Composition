db9a9f2d8c33efcdd7e2fed27ade538128d1ae38
Francois Ledoyen <ledoyen202@C302L-G20P13.png.unicaen.fr>
2025-03-02 13:49:47 +0100

    add mbarthez + lora on orangesum

---
+++ experimentations/mbarthez-lora-orangesum/run.py
@@ -0,0 +1,53 @@
+from transformers import AutoModelForSeq2SeqLM
+
+from etr_fr_expes.config import ETRDataConfig, ETRTrainingConfig, ETRTunerConfig
+from etr_fr_expes.dataset import DS_KEY_ETR_FR, DS_KEY_ORANGESUM
+from etr_fr_expes.hyperparameters import (
+    default_training_kwargs,
+    lora_config_grid_search,
+    training_kwargs_grid_search,
+)
+from etr_fr_expes.metric import (
+    METRIC_KEY_ROUGE,
+    METRIC_KEY_ROUGEL,
+    METRIC_KEY_SRB,
+)
+from expes.cli import tuner_cli
+from expes.tuner import TrainFuncFactories
+
+DS_KEY = DS_KEY_ORANGESUM
+# To be completed or import an predefined
+training_config = ETRTrainingConfig(
+    train_tasks=[DS_KEY],
+    validation_tasks=[DS_KEY],
+    test_tasks=[DS_KEY_ETR_FR, DS_KEY],
+    is_causal_lm=False,
+    data_config=ETRDataConfig(
+        tokenize_dataset=True,
+        input_max_length=400,
+        output_max_length=100,
+    ),
+    adapter_configs=lora_config_grid_search(f"lora_{DS_KEY}"),
+    adapter_activation=f"lora_{DS_KEY}",
+    model_checkpoint="facebook/mbart-large-50",
+    model_class=AutoModelForSeq2SeqLM,
+    tokenizer_kwargs={"src_lang": "fr_XX", "tgt_lang": "fr_XX"},
+    generation_config={"max_new_tokens": 100, "num_beams": 4},
+    training_kwargs={
+        **training_kwargs_grid_search(),
+        **default_training_kwargs(),
+        "num_train_epochs": 20,
+    },
+)
+tuner_config = ETRTunerConfig(
+    grace_period=10,
+    metric=f"eval_{DS_KEY}_{METRIC_KEY_ROUGEL}",
+)
+
+if __name__ == "__main__":
+    tuner_cls = tuner_cli()
+    tuner = tuner_cls(
+        tuner_config=tuner_config,
+        factories=TrainFuncFactories(training_config),
+    )
+    tuner()
