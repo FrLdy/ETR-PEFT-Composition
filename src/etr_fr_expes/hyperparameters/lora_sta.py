381811380ec4afd111ecb9c243a2f5448bd28699
Francois Ledoyen <ledoyen202@C302L-G20P13.png.unicaen.fr>
2025-04-05 20:07:38 +0200

    add mtllora hps

--- src/etr_fr_expes/hyperparameters/lora_sta.py
+++ src/etr_fr_expes/hyperparameters/lora_sta.py
@@ -1,4 +1,4 @@
-from adapters.configuration.adapter_config import LoRAConfig
+from adapters import LoRAConfig, MTLLoRAConfig
 from ray import tune
 
 from etr_fr_expes.hyperparameters.utils import sample_from_adapter
@@ -8,8 +8,23 @@ def lora_config_grid_search(adapter_name):
     return {
         adapter_name: LoRAConfig(
             attn_matrices=["q", "k", "v"],
-            r=tune.grid_search([128]),
+            r=128,
             output_lora=True,
             alpha=sample_from_adapter(adapter_name=adapter_name, param="r"),
         )
+    }
+
+def mtllora_config_grid_search(tasks):
+    def config(task):
+        return MTLLoRAConfig(
+            attn_matrices=["q", "k", "v"],
+            n_up_projection=tune.grid_search([1, 2, 3]),
+            r=128,
+            output_lora=True,
+            alpha=sample_from_adapter(adapter_name=task, param="r"),
+
+        )
+
+    return {
+        task: config(task) for task in tasks
     }
