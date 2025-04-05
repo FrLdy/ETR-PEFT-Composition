9a65806d527605ece6b4dc28b0f277b372b5595c
Francois Ledoyen <ledoyen202@C302L-G20P13.png.unicaen.fr>
2025-04-05 22:56:25 +0200

    fix mtl tune grid

--- src/etr_fr_expes/hyperparameters/lora_sta.py
+++ src/etr_fr_expes/hyperparameters/lora_sta.py
@@ -1,3 +1,5 @@
+from random import sample
+
 from adapters import LoRAConfig, MTLLoRAConfig
 from ray import tune
 
@@ -15,16 +17,18 @@ def lora_config_grid_search(adapter_name):
     }
 
 def mtllora_config_grid_search(tasks):
-    def config(task):
+    def config(task, base_task=None):
         return MTLLoRAConfig(
             attn_matrices=["q", "k", "v"],
-            n_up_projection=tune.grid_search([1, 2, 3]),
+            n_up_projection=tune.grid_search([1, 2, 3]) if base_task is None else sample_from_adapter(adapter_name=base_task, param="n_up_projection"),
             r=128,
             output_lora=True,
             alpha=sample_from_adapter(adapter_name=task, param="r"),
 
         )
 
+    base_task, tasks = tasks[0], tasks[1:]
     return {
-        task: config(task) for task in tasks
+        base_task: config(base_task),
+        **{task: config(task, base_task=base_task) for task in tasks}
     }
